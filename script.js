cb93c2cc9485685af22d4b7052183d72947b9b78
selvinjethu <60860622+selvinjethu@users.noreply.github.com>
2026-02-16 09:11:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function downloadResume() {
 print("");
 print("────────────────────────────────────────");
 print(" SELVIN RAJ • DEVOPS TERMINAL PORTFOLIO ");
-print(" Senior DevOps Engineer | Platform Engineering");
+print(" Senior DevOps Engineer | Platform Engineering | Cloud Infra Engineering | SRE");
 print("────────────────────────────────────────");
 print("");
 print("Type 'help' or choose a number:");
